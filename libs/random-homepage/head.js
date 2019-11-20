0fd9bd381376690d87576ba7772674319873fad7
不蠢会死的某翠 <idealclover@live.com>
2019-11-20 16:35:46 +0800

    :sparkles: change title

--- libs/random-homepage/head.js
+++ libs/random-homepage/head.js
@@ -77,7 +77,7 @@
 
 	function play() {
 	  var head = document.getElementById('head');
-	  var history = (0, _gen2.default)('Stay simple, stay naive.').history;
+	  var history = (0, _gen2.default)('Stay curious, stay naive.').history;
 	  isRunning = true;
 	  var i = 0;
 	  history.forEach(function (text, i) {
